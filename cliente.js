c035cc97017489a62c55973e9ad86b9ff19e5ca9
Jottaset <44384275+Jottaset@users.noreply.github.com>
2019-01-09 19:08:02 -0400

    Add files via upload

---
+++ cliente.js
@@ -0,0 +1,10 @@
+function msg(){
+    //console.log("Testando");
+
+    $.alert({
+        title: 'Atenção',
+        content: 'Cadastro Efetuado com Sucesso!',
+        type: 'red'
+    });
+    
+}
